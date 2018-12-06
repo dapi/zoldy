ee3551b9a14a638a9d2432caf1dc274a6dd5a81d
Danil Pismenny <danil@brandymint.ru>
2018-12-06 07:44:02 +0300

    remove remotes_api log

--- app/api/remotes_api.rb
+++ app/api/remotes_api.rb
@@ -4,17 +4,12 @@
 # Returns available list of <Remote> nodes
 #
 class RemotesAPI < Grape::API
-  helpers do
-    include AutoLogger::Named.new(name: :remotes_api)
-  end
-
   format :json
   default_format :json
   formatter :json, PrettyJSONFormatter
 
   desc 'Return remotes list'
   get :remotes do
-    logger.info "Get remotes with headers #{zold_headers}"
     zold_present all: Zoldy.app.remotes_store.remotes.map(&:as_json)
   end
 end
