12815f6e79341ba00ba05eec73f623f35da06bd3
Danil Pismenny <danil@brandymint.ru>
2018-12-31 22:47:59 +0300

    Fix WalletPusher

--- app/workers/wallet_pusher.rb
+++ app/workers/wallet_pusher.rb
@@ -8,30 +8,33 @@ class WalletPusher
   include AutoLogger
 
   def perform(wallet_id, node_alias = nil)
-    if node_alias.present?
-      safe_push_wallet wallet_id, node_alias
-    else
-      Zoldy.app.remotes_store.alive.each do |remote|
-        WalletPusher.perform_async wallet_id, remote
-      end
-    end
-  end
+    return push_wallet_to_all_nodes wallet_id unless node_alias.present?
 
-  private
-
-  def safe_push_wallet(wallet_id, node_alias)
     unless Zoldy.app.remotes_store.alive? node_alias
       logger.warn "Ignore dead node #{node_alias}"
       return
     end
-    push_wallet wallet_id, node_alias
+
+    safe_push_wallet wallet_id, node_alias
+  end
+
+  private
+
+  def push_wallet_to_all_nodes(wallet_id)
+    Zoldy.app.remotes_store.alive.each do |remote|
+      self.class.perform_async wallet_id, remote
+    end
+  end
+
+  def safe_push_wallet(wallet_id, node_alias)
+    wallet = Zoldy.app.wallets_store.find! wallet_id
+    push_wallet wallet, node_alias
   rescue ZoldClient::Error => ex
     logger.info "Push #{wallet} (#{wallet.digest}) to #{node_alias} failed with #{ex}"
     Zoldy.app.remotes_store.add_error node_alias, ex
   end
 
-  def push_wallet(wallet_id, node_alias)
-    wallet = Zoldy.app.wallets_store.find! wallet_id
+  def push_wallet(wallet, node_alias)
     client = ZoldClient.new node_alias
     result = client.push_wallet wallet
     logger.info "Push #{wallet} (#{wallet.digest}) to #{node_alias}: #{result}"
